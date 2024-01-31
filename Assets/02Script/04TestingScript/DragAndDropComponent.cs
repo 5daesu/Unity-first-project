7f3be92dc6764645affcd2df213f39b4df1a8e57
5daesu <100927715+5daesu@users.noreply.github.com>
2024-01-31 12:40:20 +0900

    Revert "github_test2"
    This reverts commit d22a15124e05475084fca6bb02b493f2bc5b5511.

--- Assets/02Script/04TestingScript/DragAndDropComponent.cs
+++ Assets/02Script/04TestingScript/DragAndDropComponent.cs
@@ -18,7 +18,6 @@ public class DragAndDropComponent : MonoBehaviour, IPointerDownHandler, IBeginDr
     private CanvasGroup canvasGroup;
     private CanvasGroup bo_canvasGroup;
 
-<<<<<<< HEAD
     private void Start()
     {
 <<<<<<< HEAD
@@ -86,10 +85,6 @@ public class DragAndDropComponent : MonoBehaviour, IPointerDownHandler, IBeginDr
         //unitData = gameObject.transform.parent.transform.parent.GetComponent<TreeNodeObject>().unitData;
 =======
 >>>>>>> parent of 53ca4c0 (1/31 ver.1)
-=======
-    private void Start()
-    {
->>>>>>> parent of 53ca4c0 (1/31 ver.1)
         rectTransform = GetComponent<RectTransform>();
         bi_rectTransform = blankImage.GetComponent<RectTransform>();
         canvas = GetComponent<Canvas>();
@@ -136,12 +131,7 @@ public class DragAndDropComponent : MonoBehaviour, IPointerDownHandler, IBeginDr
 
     }
 <<<<<<< HEAD
-<<<<<<< HEAD
 */
-=======
-
-}
->>>>>>> parent of 53ca4c0 (1/31 ver.1)
 =======
 
 }
