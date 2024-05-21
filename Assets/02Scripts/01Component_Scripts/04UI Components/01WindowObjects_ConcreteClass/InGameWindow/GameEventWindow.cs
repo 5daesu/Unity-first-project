cc27a1e2bfed945fa66cc47f3d7498dc0f02f1c3
5daesu <100927715+5daesu@users.noreply.github.com>
2024-05-22 00:56:55 +0900

    24/05/22 Commit
    Changing Folder Name
    Changing File Path
    
    Delete File
    FixedToggleWindow
    FloatingToggleWindow

--- Assets/02Scripts/01Component_Scripts/04UI Components/01WindowObjects_ConcreteClass/InGameWindow/GameEventWindow.cs
+++ Assets/02Scripts/01Component_Scripts/04UI Components/01WindowObjects_ConcreteClass/InGameWindow/GameEventWindow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameEventWindow : FixedToggleWindow
+public class GameEventWindow : TogglingWindow
 {
     [SerializeField] private Image gameEventImage;
     [SerializeField] private Text gameEventName;
